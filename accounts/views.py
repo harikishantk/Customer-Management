aa67fe7e54a4a3488fa1135a9ba78fd0339709c2
Harikishan TK <58771359+harikishantk@users.noreply.github.com>
2021-05-27 12:04:15 +0530

    CRUD with Model Forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 def home(request):
@@ -24,3 +25,35 @@ def customers(request, pk_test):
     orders_count = orders.count()
     context = {'customers': customers, 'orders': orders, 'orders_count': orders_count}
     return render(request, 'accounts/customers.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        print('Printing POST:',request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        print('Printing POST:',request.POST)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+        
+    context = {'item': order}
+    return render(request, 'accounts/delete.html', context)
