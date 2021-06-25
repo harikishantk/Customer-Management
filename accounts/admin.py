776298eec9be4b36ba8563b787b9ab139ecdb576
Harikishan TK <58771359+harikishantk@users.noreply.github.com>
2021-06-25 13:43:26 +0530

    User Registration and Login Authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 
+# Register your models here.
+
 from .models import *
 
-# Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
-
-class OrderShow(admin.ModelAdmin):
-    list_display = ['productName','orderedBy']
-    class Meta:
-        model = Order
-
-admin.site.register(Order, OrderShow)
 admin.site.register(Tag)
+admin.site.register(Order)
